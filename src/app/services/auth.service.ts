33c8b1b590e52c4a002558d8276199296af83814
Vianka Medina Balbi <54475761+ViankaMB@users.noreply.github.com>
2021-06-05 12:16:11 -0400

    user inteface created, services added

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,70 @@
 import { Injectable } from '@angular/core';
+import { User } from '../shared/user.interface';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) { }
+ 
+  async resetPassword(email: string): Promise<void> { 
+    try {
+      return this.afAuth.sendPasswordResetEmail(email);
+    } catch (error) {
+      console.log('Error -> ', error)
+    }
+  }
+
+
+  async loginGoogle(): Promise<User> {
+    try {
+      const { user } = await this.afAuth.signInWithPopup(new auth.GoogleAuthProvider());
+      return user;
+    } catch (error) {
+      console.log('Error -> ', error)
+    }
+  }
+
+
+  async register(email: string, password: string): Promise<User> {
+    try {
+      const { user } = await this.afAuth.createUserWithEmailAndPassword(email, password);
+      await this.sendVerificationEmail();
+      return user;
+    } catch (error) {
+      console.log('Error -> ', error)
+    }
+  }
+
+
+  async login(email: string, password: string): Promise<User> {
+    try {
+      const { user } = await this.afAuth.signInWithEmailAndPassword(email, password);
+      return user;
+    } catch (error) {
+      console.log('Error -> ', error)
+    }
+  }
+
+  async sendVerificationEmail(): Promise<void> {
+    try {
+      return (await this.afAuth.currentUser).sendEmailVerification();
+    } catch (error) {
+      console.log('Error -> ', error)
+    }
+  }
+
+  async logout(): Promise<void> {
+    try {
+      await this.afAuth.signOut();
+    } catch (error) {
+      console.log('Error -> ', error)
+    }
+  }
+
+
+
 }
