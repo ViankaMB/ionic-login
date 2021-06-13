7dda53d2a40f7d5670a8725c0394a82cfd80874c
Vianka Medina Balbi <54475761+ViankaMB@users.noreply.github.com>
2021-06-13 15:23:52 -0400

    services config completed

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,14 +3,30 @@ import { User } from '../shared/user.interface';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase/app';
 
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private afAuth: AngularFireAuth) { }
- 
-  async resetPassword(email: string): Promise<void> { 
+  public user$: Observable<User>;
+
+  constructor(private afAuth: AngularFireAuth, private afs: AngularFirestore) { 
+    this.user$ = this.afAuth.authState.pipe(
+      switchMap((user) => {
+        if (user) {
+          return this.afs.doc<User>(`users/${user.uid}`).valueChanges();
+        }
+        return of(null);
+      })
+    );
+  }
+
+  async resetPassword(email: string): Promise<void> {
     try {
       return this.afAuth.sendPasswordResetEmail(email);
     } catch (error) {
@@ -22,6 +38,7 @@ export class AuthService {
   async loginGoogle(): Promise<User> {
     try {
       const { user } = await this.afAuth.signInWithPopup(new auth.GoogleAuthProvider());
+      this.updateUserData(user);
       return user;
     } catch (error) {
       console.log('Error -> ', error)
@@ -43,6 +60,7 @@ export class AuthService {
   async login(email: string, password: string): Promise<User> {
     try {
       const { user } = await this.afAuth.signInWithEmailAndPassword(email, password);
+      this.updateUserData(user);
       return user;
     } catch (error) {
       console.log('Error -> ', error)
@@ -65,6 +83,18 @@ export class AuthService {
     }
   }
 
+
+  private updateUserData(user: User) {
+    const userRef: AngularFirestoreDocument<User> = this.afs.doc(`users/${user.uid}`);
+    const data: User = {
+      uid: user.uid,
+      email: user.email,
+      emailVerified: user.emailVerified,
+      displayName: user.displayName,
+    };
+
+    return userRef.set(data, { merge: true });
+  }
 
 
 }
